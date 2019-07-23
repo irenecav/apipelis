ad107ed8d52a95fdd358b5289594670f384cda33
Irene Rosell <irenerosellg@gmail.com>
2019-07-23 23:53:33 +0200

    Create a director model

---
+++ models/Director.js
@@ -0,0 +1,14 @@
+
+const mongoose = require('mongoose')
+
+const directorSchema = mongoose.Schema({
+    name: { type: String, index: true }
+}, { collection: 'directores'})
+
+const Director = mongoose.model('Director', directorSchema)
+
+Director.find().exec((err, directores) => {
+    console.log(directores)
+})
+
+module.exports = Director
---
+++ routes/directores.js
@@ -0,0 +1,10 @@
+
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+  const directores = await Director.find()
+   ihrejnwkjuvfhejwnkjou
+})
+
+module.exports = router
